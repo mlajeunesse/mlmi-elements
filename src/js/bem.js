41df96df8f6596c3c6514094840c247f29b4b944
Mathieu Lajeunesse <contact@mathieulajeunesse.com>
2018-07-29 23:14:37 -0400

    v0.2

---
+++ src/js/bem.js
@@ -0,0 +1,41 @@
+import $ from 'jquery';
+
+/*
+*	BlockElement
+*/
+
+$.fn.BlockElement = function(_block_class)
+{
+	var _el = this;
+	_el.block_class = _block_class;
+
+	_el.getBlockClass = function()
+	{
+		return _el.block_class;
+	};
+
+	_el.addBlockElement = function(_element_class, _element)
+	{
+		if (_element == undefined) _element = "div";
+		var newBlockElement = $('<' + _element + '>').BlockElement(_el.getBlockClass() + "__" + _element_class);
+		return newBlockElement;
+	};
+
+	_el.addModifier = function(_modifier_class)
+	{
+		_el.addClass(_el.block_class + "--" + _modifier_class);
+		return _el;
+	};
+
+	_el.removeModifier = function(_modifier_class)
+	{
+		_el.removeClass(_el.block_class + "--" + _modifier_class);
+		return _el;
+	};
+
+	return function()
+	{
+		_el.addClass(_el.block_class)
+		return _el;
+	}();
+}
