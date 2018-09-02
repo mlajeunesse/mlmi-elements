1d2cc11a92dbc80ba78e4f4171e75f96ed546f2c
Mathieu Lajeunesse <contact@mathieulajeunesse.com>
2018-09-02 14:23:02 -0400

    v0.3.4 — Add 'toggle' event on window for mobile toggle

--- modules/mobile.js
+++ modules/mobile.js
@@ -15,21 +15,25 @@ export default function () {
       if (obj.mobileCallback != undefined){
         obj.mobileCallback();
       }
+      window.dispatchEvent(new CustomEvent('toggle'));
 		} else if ((obj.isMobile === -1 || obj.isMobile) && "matchMedia" in window && window.matchMedia("(min-width: 768px)").matches) {
 			obj.isMobile = false;
       if (obj.desktopCallback != undefined){
         obj.desktopCallback();
       }
+      window.dispatchEvent(new CustomEvent('toggle'));
 		}
 	}
 
   /* Add callbacks */
-  this.check = function(_mobileCallback, _desktopCallback) {
+  this.addCallbacks = function(_mobileCallback, _desktopCallback) {
     obj.mobileCallback = _mobileCallback;
     obj.desktopCallback = _desktopCallback;
-    $(window).on("resize orientationchange load", obj.resized);
-    obj.resized();
+    return obj;
   }
 
+  /* Initializer */
+  $(window).on("resize orientationchange load", obj.resized);
+  obj.resized();
   return this;
 }
