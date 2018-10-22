fe9d0bd7650c7ff9f1e453571a51ef8f0a8f2434
Mathieu Lajeunesse <contact@mathieulajeunesse.com>
2018-10-22 12:48:16 -0400

    v0.3.10 — Add default Utils class with extend method

--- utils.js
+++ utils.js
@@ -1,3 +1,15 @@
 export const TRANSITION_END = "webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend";
 
 export const ANIMATION_END = "webkitAnimationEnd oanimationend oAnimationEnd msAnimationEnd animationend";
+
+export default {
+  extend: function(object, properties) {
+    var newObject = Object.create(object);
+    for (var property in properties){
+      if (properties.hasOwnProperty(property)){
+        newObjectpropertyprop] = properties[property];
+      }
+    }
+    return newObject;
+  }
+}
