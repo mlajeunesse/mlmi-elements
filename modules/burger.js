a6f9603ceb0036504f5da0f7a7791b93cbc8c65e
Mathieu Lajeunesse <contact@mathieulajeunesse.com>
2019-04-03 15:29:28 -0400

    v0.4.10 — Add burger element

---
+++ modules/burger.js
@@ -0,0 +1,62 @@
+/*
+*	Burger
+*/
+
+export default function (selector, options)
+{
+  var obj = this;
+  this.isExpanded = false;
+
+  /*
+  * Options
+  */
+  this.options = $.extend({
+    navigation: "#navigation",
+    onToggle: undefined,
+  }, options);
+
+  $.fn.Burger = function() {
+    let self = this;
+    self.navigation = $(obj.options.navigation);
+
+    self.clicked = function() {
+      if (self.attr("aria-expanded") == "false"){
+        self.attr("aria-expanded", "true");
+      } else {
+        self.attr("aria-expanded", "false");
+      }
+      obj.isExpanded = (self.attr("aria-expanded") == "true");
+      self.trigger('burger_clicked');
+    };
+
+    self.maybe_close = function() {
+      if (self.attr("aria-expanded") == "true"){
+        self.attr("aria-expanded", "false");
+      }
+    };
+
+    return function() {
+      self.on("click", self.clicked);
+      obj.isExpanded = (self.attr("aria-expanded") == "true");
+      return self;
+    }();
+  };
+
+  /* Initializer */
+	$(selector).each(function(){
+    let burger = $(this).Burger();
+    burger.on('burger_clicked', function() {
+      if (obj.options.onToggle != undefined) {
+        obj.options.onToggle();
+      }
+    });
+	});
+
+  return obj;
+}
+
+// <button class="burger" aria-label="<?php _e('Navigation', 'mlmi') ?>" aria-expanded="false" aria-controls="navigation">
+//   <div class="burger__el burger__el--top"></div>
+//   <div class="burger__el burger__el--middle"></div>
+//   <div class="burger__el burger__el--bottom"></div>
+// </button>
