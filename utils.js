596a86107ea88e5d47817a80c821c591aae9acf6
Mathieu Lajeunesse <contact@mathieulajeunesse.com>
2020-04-07 12:01:49 -0400

    v0.4.35 — Improve email validation util

--- utils.js
+++ utils.js
@@ -1,57 +1,57 @@
-export const TRANSITION_END = "webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend";
+export const TRANSITION_END = "webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend"
 
-export const ANIMATION_END = "webkitAnimationEnd oanimationend oAnimationEnd msAnimationEnd animationend";
+export const ANIMATION_END = "webkitAnimationEnd oanimationend oAnimationEnd msAnimationEnd animationend"
 
 export default {
   extend: function(object, properties) {
-    let newObject = Object.create(object);
+    let newObject = Object.create(object)
     for (let property in properties){
-      newObject[property] = properties[property];
+      newObject[property] = properties[property]
     }
-    return newObject;
+    return newObject
   },
 }
 
 /* Validate postal code */
 export function validatePostalCode(postalCode) {
-  return /[A-Z][0-9][A-Z]( )?[0-9][A-Z][0-9]/.test(postalCode.toUpperCase());
+  return /[A-Z][0-9][A-Z]( )?[0-9][A-Z][0-9]/.test(postalCode.toUpperCase())
 }
 
 /* Validate email address */
 export function validateEmail(emailAddress) {
-  return /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(emailAddress)
+  return /^\w+([.+-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(emailAddress)
 }
 
 /* Set cookie */
 export function setCookie(cookie_name, cookie_value, seconds) {
-  let cookieTime = new Date();
-  cookieTime.setTime(cookieTime.getTime() + (seconds * 1000));
-  document.cookie = cookie_name + "=" + cookie_value + ";" + "expires=" + cookieTime.toUTCString() + ";path=/";
+  let cookieTime = new Date()
+  cookieTime.setTime(cookieTime.getTime() + (seconds * 1000))
+  document.cookie = cookie_name + "=" + cookie_value + ";" + "expires=" + cookieTime.toUTCString() + ";path=/"
 }
 
 /* Read cookie */
 export function getCookie(cookie_name) {
-  let name = cookie_name + "=";
-  let ca = document.cookie.split(';');
+  let name = cookie_name + "="
+  let ca = document.cookie.split(';')
   for (let i = 0; i < ca.length; i++) {
-    let c = ca[i];
+    let c = ca[i]
     while (c.charAt(0) == ' ') {
-      c = c.substring(1);
+      c = c.substring(1)
     }
     if (c.indexOf(name) == 0) {
-      return c.substring(name.length, c.length);
+      return c.substring(name.length, c.length)
     }
   }
-  return "";
+  return ''
 }
 
 /* Read GET parameter */
 export function getParameter(parameterName) {
-  let result = false, tmp = [];
-  let items = location.search.substr(1).split("&");
+  let result = false, tmp = []
+  let items = location.search.substr(1).split("&")
   for (let index = 0; index < items.length; index++) {
-    tmp = items[index].split("=");
-    if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
+    tmp = items[index].split("=")
+    if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1])
   }
-  return result;
+  return result
 }
