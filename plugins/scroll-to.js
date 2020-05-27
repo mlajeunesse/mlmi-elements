ad06376121058c2ac5bc3a633bed0f81279713c7
Mathieu Lajeunesse <contact@mathieulajeunesse.com>
2020-05-27 16:50:45 -0400

    v0.4.36 — Add easing parameter to scrollTo (default "swing")

--- plugins/scroll-to.js
+++ plugins/scroll-to.js
@@ -1,7 +1,7 @@
 /*
 * Animated scroll to
 */
-$.fn.scrollTo = function(target, baseDuration, distanceQuotient) {
+$.fn.scrollTo = function(target, baseDuration, distanceQuotient, easing) {
   /* Default values */
   if (typeof baseDuration == 'undefined') {
     baseDuration = 500
@@ -9,10 +9,13 @@ $.fn.scrollTo = function(target, baseDuration, distanceQuotient) {
   if (typeof distanceQuotient == 'undefined') {
     distanceQuotient = 4
   }
+  if (typeof easing == 'undefined') {
+    easing = 'swing'
+  }
 
   /* Scroll animated */
   let distance = Math.abs(target - $(this).scrollTop())
-  $(this).animate({'scrollTop' : target}, baseDuration + distance / distanceQuotient, 'swing')
+  $(this).animate({'scrollTop' : target}, baseDuration + distance / distanceQuotient, easing)
 
   /* Allow breaking of scroll on mousewheel */
   $(this).one('mousewheel', function() {
