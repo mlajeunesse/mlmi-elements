d22fd5bcfdeda8bab513122cdb7b83c784047008
Mathieu Lajeunesse <contact@mathieulajeunesse.com>
2020-06-10 22:13:03 -0400

    v0.5.4 — Add formatDate util function for yyyy-mm-dd output

--- utils.js
+++ utils.js
@@ -55,3 +55,10 @@ export function getParameter(parameterName) {
   }
   return result
 }
+
+export function formatDate(date) {
+  let d = new Date(date), month = '' + (d.getMonth() + 1), day = '' + d.getDate(), year = d.getFullYear();
+  if (month.length < 2) month = '0' + month;
+  if (day.length < 2) day = '0' + day;
+  return [year, month, day].join('-');
+}
